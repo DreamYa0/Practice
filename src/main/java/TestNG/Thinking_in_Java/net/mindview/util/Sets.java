ca50ce9efd47b4ec89c7ec0217ef2e874b74498b
yaoijun <yaojun@ebigdata.org>
2016-10-15 23:59:47 +0800

    增加Sets类

---
+++ src/main/java/TestNG/Thinking_in_Java/net/mindview/util/Sets.java
@@ -0,0 +1,33 @@
+package TestNG.Thinking_in_Java.net.mindview.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author DreamYao
+ *         Created by DreamYao on 2016/10/15.
+ *         P367 一个Set实用工具
+ */
+public class Sets {
+    public static <T> Set<T> union(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<>(a);
+        result.addAll(b);
+        return result;
+    }
+
+    public static <T> Set<T> intersection(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<>(a);
+        result.retainAll(b);
+        return result;
+    }
+
+    public static <T> Set<T> difference(Set<T> superset, Set<T> subset) {
+        Set<T> result = new HashSet<>(superset);
+        result.removeAll(subset);
+        return result;
+    }
+
+    public static <T> Set<T> complement(Set<T> a, Set<T> b) {
+        return difference(union(a, b), intersection(a, b));
+    }
+}

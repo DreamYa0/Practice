205ef4c92c8cf56d62c2c222609cec046f4a73ec
DreamYao <805229888@qq.com>
2016-10-23 23:13:18 +0800

    加入mybatis

---
+++ src/main/java/mybatis/SqlSessionFactoryUtil.java
@@ -0,0 +1,13 @@
+package mybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+/**
+ * @author DreamYao
+ *         Created by DreamYao on 2016/10/23.
+ *         P31
+ */
+public class SqlSessionFactoryUtil {
+    //SqlSessionFactory对象
+    private static SqlSessionFactory sqlSessionFactory = null;
+}

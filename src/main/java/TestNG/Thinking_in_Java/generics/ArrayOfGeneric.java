a10eec59b1a347553de31c48cba28b44f28514c7
DreamYao <805229888@qq.com>
2016-11-08 22:46:33 +0800

    新增类

---
+++ src/main/java/TestNG/Thinking_in_Java/generics/ArrayOfGeneric.java
@@ -0,0 +1,18 @@
+package TestNG.Thinking_in_Java.generics;
+
+
+/**
+ * @author DreamYao
+ *         Created by DreamYao on 2016/11/8.
+ *         P384 泛型数组
+ */
+public class ArrayOfGeneric {
+    static final int SIZE = 100;
+    static Generic<Integer>[] gia;
+
+    public static void main(String[] args) {
+        gia = new Generic[SIZE];
+        System.out.println(gia.getClass().getSimpleName());
+        gia[0] = new Generic<>();
+    }
+}
--- src/main/java/TestNG/Thinking_in_Java/generics/FactoryConstraint.java
+++ src/main/java/TestNG/Thinking_in_Java/generics/FactoryConstraint.java
@@ -1,17 +1,43 @@
 package TestNG.Thinking_in_Java.generics;
 
 /**
- * Created by DreamYao on 2016/11/6.
+ * @author DreamYao
+ *         Created by DreamYao on 2016/11/6.
+ *         P382 工厂对象
  */
 public class FactoryConstraint {
+    public static void main(String[] args) {
+        new Foo2<>(new IntegerFactory());
+        new Foo2<>(new Widget.Factory());
+    }
 }
-interface FactoryI<T>{
+
+interface FactoryI<T> {
     T create();
 }
-class Foo2<T>{
+
+class Foo2<T> {
     private T x;
 
-    private <F extends FactoryI<T>> Foo2(F factory) {
-        x=factory.create();
+    public <F extends FactoryI<T>> Foo2(F factory) {
+        x = factory.create();
+    }
+}
+
+class IntegerFactory implements FactoryI<Integer> {
+
+    @Override
+    public Integer create() {
+        return new Integer(0);
+    }
+}
+
+class Widget {
+    public static class Factory implements FactoryI<Widget> {
+
+        @Override
+        public Widget create() {
+            return new Widget();
+        }
     }
 }

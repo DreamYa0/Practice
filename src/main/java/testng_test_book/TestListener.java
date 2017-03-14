43d489877d25e9544c73d5d8aaf8dffe885ff732
DreamYao <805229888@qq.com>
2017-03-14 22:48:20 +0800

    TestNG监听器

---
+++ src/main/java/testng_test_book/TestListener.java
@@ -0,0 +1,25 @@
+package testng_test_book;
+
+import org.testng.IMethodInstance;
+import org.testng.IMethodInterceptor;
+import org.testng.ITestContext;
+
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by DreamYao on 2017/3/14.
+ */
+public class TestListener implements IMethodInterceptor {
+
+    @Override
+    public List<IMethodInstance> intercept(List<IMethodInstance> methods, ITestContext context) {
+        ListIterator<IMethodInstance> iterator=methods.listIterator();
+        while (iterator.hasNext()) {
+            IMethodInstance iMethodInstance=iterator.next();
+            Object object=iMethodInstance.getInstance();
+        }
+        String name=context.getName();
+        return null;
+    }
+}

1e7ffa3838491a3c549ed2d66fbeb0fa335b1870
DreamYao <805229888@qq.com>
2018-03-15 18:14:45 +0800

    代码提交

--- src/main/java/netty/EchoServer.java
+++ src/main/java/netty/EchoServer.java
@@ -3,10 +3,13 @@ package netty;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 import javax.annotation.concurrent.Immutable;
 import java.net.InetSocketAddress;
@@ -40,18 +43,23 @@ public class EchoServer {
 
     public void start() throws Exception {
         final EchoServerHandler serverHandler = new EchoServerHandler();
-        EventLoopGroup group = new NioEventLoopGroup();
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
             // 指定所使用的NIO传输Channel
-            b.group(group)
+            b.group(bossGroup,workerGroup)
                     .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,100)
                     .localAddress(new InetSocketAddress(port))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             // EchoServerHandler被标注为@Shareable，所以我们可以总是使用同样的实列
-                            ch.pipeline().addLast(serverHandler);
+                            ch.pipeline()
+                                    .addLast(new DelimiterBasedFrameDecoder(1024))
+                                    .addLast(new StringDecoder())
+                                    .addLast(serverHandler);
                         }
                     });
             // 异步地绑定服务器调用sync()方法阻塞等待直到绑定完成
@@ -60,7 +68,7 @@ public class EchoServer {
             future.channel().closeFuture().sync();
         } finally {
             // 关闭EventLoopGroup释放所有的资源
-            group.shutdownGracefully().sync();
+            bossGroup.shutdownGracefully().sync();
         }
     }
 }
